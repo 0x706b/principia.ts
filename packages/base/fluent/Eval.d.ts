f0ee24e78061ae6222f682b952075265b357ef0a
0x706b <peter@peterkrol.com>
2021-09-25 00:48:41 -0400

    feat(base/eval): add fluent static definitions for `Eval`

--- packages/base/fluent/Eval.d.ts
+++ packages/base/fluent/Eval.d.ts
@@ -1,3 +1,82 @@
+import type * as Ev from '@principia/base/Eval'
+
+/* eslint typescript-sort-keys/interface: "error" */
+
+declare global {
+  export const Eval: EvalStaticOps
+  export interface Eval<A> extends Ev.Eval<A> {}
+}
+
+export interface EvalStaticOps {
+  /**
+   * @rewriteStatic Applicative from "@principia/base/Eval"
+   */
+  Applicative: typeof Ev.Applicative
+  /**
+   * @rewriteStatic Functor from "@principia/base/Eval"
+   */
+  Functor: typeof Ev.Functor
+  /**
+   * @rewriteStatic Monad from "@principia/base/Eval"
+   */
+  Monad: typeof Ev.Monad
+  /**
+   * @rewriteStatic MonoidalFunctor from "@principia/base/Eval"
+   */
+  MonoidalFunctor: typeof Ev.MonoidalFunctor
+  /**
+   * @rewriteStatic SemimonoidalFunctor from "@principia/base/Eval"
+   */
+  SemimonoidalFunctor: typeof Ev.SemimonoidalFunctor
+  /**
+   * @rewriteStatic always from "@principia/base/Eval"
+   */
+  always: typeof Ev.always
+  /**
+   * @rewriteStatic apS from "@principia/base/Eval"
+   */
+  apS: typeof Ev.apS
+  /**
+   * @rewriteStatic apT from "@principia/base/Eval"
+   */
+  apT: typeof Ev.apT
+  /**
+   * @rewriteStatic defer from "@principia/base/Eval"
+   */
+  defer: typeof Ev.defer
+  /**
+   * @rewriteStatic gen from "@principia/base/Eval"
+   */
+  gen: typeof Ev.gen
+  /**
+   * @rewriteStatic later from "@principia/base/Eval"
+   */
+  later: typeof Ev.later
+  /**
+   * @rewriteStatic mapN from "@principia/base/Eval"
+   */
+  mapN: typeof Ev.mapN_
+  /**
+   * @rewriteStatic now from "@principia/base/Eval"
+   */
+  now: typeof Ev.now
+  /**
+   * @rewriteStatic pure from "@principia/base/Eval"
+   */
+  pure: typeof Ev.pure
+  /**
+   * @rewriteStatic sequenceS from "@principia/base/Eval"
+   */
+  sequenceS: typeof Ev.sequenceS
+  /**
+   * @rewriteStatic sequenceT from "@principia/base/Eval"
+   */
+  sequenceT: typeof Ev.sequenceT
+  /**
+   * @rewriteStatic unit from "@principia/base/Eval"
+   */
+  unit: typeof Ev.unit
+}
 declare module '@principia/base/Eval/core' {
   interface Eval<A> {
     /**
