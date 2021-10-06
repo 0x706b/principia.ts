b52224be09623392db79efa46008dac0e8a42621
0x706b <peter@peterkrol.com>
2021-10-05 23:40:45 -0400

    feat(express): improve types

--- packages/express/src/index.ts
+++ packages/express/src/index.ts
@@ -2,20 +2,22 @@
 
 import type { Has } from '@principia/base/Has'
 import type { Cause } from '@principia/base/IO/Cause'
-import type { NonEmptyArray } from '@principia/base/NonEmptyArray'
 import type { _A, _R } from '@principia/base/util/types'
 import type { NextHandleFunction } from 'connect'
 import type { Express, NextFunction, Request, RequestHandler, Response } from 'express'
+import type { RouteParameters } from 'express-serve-static-core'
 import type { Server } from 'http'
 
 import '@principia/base/Operators'
 
+import * as A from '@principia/base/Array'
+import { pipe } from '@principia/base/function'
 import { tag } from '@principia/base/Has'
-import * as T from '@principia/base/IO'
+import * as IO from '@principia/base/IO'
 import { defaultPrettyPrint, halted } from '@principia/base/IO/Cause'
-import * as F from '@principia/base/IO/Fiber'
+import * as Fi from '@principia/base/IO/Fiber'
 import * as L from '@principia/base/IO/Layer'
-import * as M from '@principia/base/IO/Managed'
+import * as Ma from '@principia/base/IO/Managed'
 import * as Supervisor from '@principia/base/IO/Supervisor'
 import { AtomicBoolean } from '@principia/base/util/support/AtomicBoolean'
 import express from 'express'
@@ -30,7 +32,7 @@ export class NodeServerListenError {
   constructor(readonly error: Error) {}
 }
 
-export const ExpressAppConfigTag = '@principia/express/AppConfig'
+export const ExpressAppConfigTag = '@principia/express/ExpressAppConfig'
 
 export interface ExpressAppConfig {
   readonly _tag: typeof ExpressAppConfigTag
@@ -44,14 +46,14 @@ export const ExpressAppConfig = tag<ExpressAppConfig>().setKey(ExpressAppConfigT
 export function LiveExpressAppConfig<R>(
   host: string,
   port: number,
-  exitHandler: (req: Request, res: Response, next: NextFunction) => (cause: Cause<never>) => T.URIO<R, void>
+  exitHandler: (req: Request, res: Response, next: NextFunction) => (cause: Cause<never>) => IO.URIO<R, void>
 ) {
   return L.fromIO(ExpressAppConfig)(
-    T.asks((r: R) => ({
+    IO.asks((r: R) => ({
       _tag: ExpressAppConfigTag,
       host,
       port,
-      exitHandler: (req, res, next) => (cause) => T.giveAll_(exitHandler(req, res, next)(cause), r)
+      exitHandler: (req, res, next) => (cause) => IO.giveAll_(exitHandler(req, res, next)(cause), r)
     }))
   )
 }
@@ -59,22 +61,20 @@ export function LiveExpressAppConfig<R>(
 export const ExpressAppTag = '@principia/express/App'
 export type ExpressAppTag = typeof ExpressAppTag
 
-export type ManagedExpressApp = M.Managed<
+export type ManagedExpressApp = Ma.Managed<
   Has<ExpressAppConfig>,
   never,
   {
     readonly _tag: ExpressAppTag
     readonly app: Express
-    readonly supervisor: Supervisor.Supervisor<readonly F.RuntimeFiber<any, any>[]>
+    readonly supervisor: Supervisor.Supervisor<readonly Fi.RuntimeFiber<any, any>[]>
     readonly server: Server
-    readonly runtime: <Handlers extends NonEmptyArray<EffectRequestHandler<any, any, any, any, any, any>>>(
+    readonly runtime: <Handlers extends IORequestHandlerArray>(
       handlers: Handlers
-    ) => T.IO<
+    ) => IO.IO<
       _R<
         {
-          [k in keyof Handlers]: [Handlers[k]] extends [EffectRequestHandler<infer R, any, any, any, any, any>]
-            ? T.URIO<R, void>
-            : never
+          [k in keyof Handlers]: [Handlers[k]] extends [AnyIORequestHandler<infer R>] ? IO.URIO<R, void> : never
         }[number]
       >,
       never,
@@ -83,24 +83,24 @@ export type ManagedExpressApp = M.Managed<
   }
 >
 
-export const makeExpressApp: ManagedExpressApp = M.gen(function* (_) {
+export const makeExpressApp: ManagedExpressApp = Ma.gen(function* (_) {
   const open = yield* _(
-    T.succeedLazy(() => new AtomicBoolean(true))['|>'](M.bracket((_) => T.succeedLazy(() => _.set(false))))
+    IO.succeedLazy(() => new AtomicBoolean(true))['|>'](Ma.bracket((_) => IO.succeedLazy(() => _.set(false))))
   )
 
-  const app = yield* _(T.succeedLazy(() => express()))
+  const app = yield* _(IO.succeedLazy(() => express()))
 
   const { exitHandler, host, port } = yield* _(ExpressAppConfig)
 
   const server = yield* _(
-    M.bracket_(
-      T.async<unknown, never, Server>((cb) => {
+    Ma.bracket_(
+      IO.async<unknown, never, Server>((cb) => {
         const onError = (err: Error) => {
-          cb(T.halt(new NodeServerListenError(err)))
+          cb(IO.halt(new NodeServerListenError(err)))
         }
         const server = app.listen(port, host, () => {
           cb(
-            T.succeedLazy(() => {
+            IO.succeedLazy(() => {
               server.removeListener('error', onError)
               return server
             })
@@ -109,42 +109,45 @@ export const makeExpressApp: ManagedExpressApp = M.gen(function* (_) {
         server.addListener('error', onError)
       }),
       (server) =>
-        T.async<unknown, never, void>((cb) => {
+        IO.async<unknown, never, void>((cb) => {
           server.close((err) => {
             if (err) {
-              cb(T.halt(new NodeServerCloseError(err)))
+              cb(IO.halt(new NodeServerCloseError(err)))
             } else {
-              cb(T.unit())
+              cb(IO.unit())
             }
           })
         })
     )
   )
 
-  const supervisor = yield* _(Supervisor.track['|>'](M.bracket((s) => s.value['|>'](T.chain(F.interruptAll)))))
+  const supervisor = yield* _(
+    pipe(
+      Supervisor.track,
+      Ma.bracket((s) => pipe(s.value, IO.chain(Fi.interruptAll)))
+    )
+  )
 
-  function runtime<
-    Handlers extends NonEmptyArray<EffectRequestHandler<any, any, any, any, any, any>>
-  >(handlers: Handlers) {
-    return T.map_(
-      T.runtime<
+  function runtime<Handlers extends IORequestHandlerArray>(handlers: Handlers) {
+    return pipe(
+      IO.runtime<
         _R<
           {
-            [k in keyof Handlers]: [Handlers[k]] extends [EffectRequestHandler<infer R, any, any, any, any, any>]
-              ? T.URIO<R, void>
-              : never
+            [k in keyof Handlers]: [Handlers[k]] extends [AnyIORequestHandler<infer R>] ? IO.URIO<R, void> : never
           }[number]
         >
-      >()['|>'](T.map((r) => r.supervised(supervisor))),
-      (r) =>
-        handlers.map(
+      >(),
+      IO.map((r) => r.supervised(supervisor)),
+      IO.map((r) =>
+        A.map_(
+          handlers,
           (handler): RequestHandler =>
-            (req, res, next) => {
+            (req, res, next) =>
               r.runFiber(
-                T.onTermination_(open.get ? handler(req, res, next) : T.interrupt, exitHandler(req, res, next))
+                IO.onTermination_(open.get ? handler(req, res, next) : IO.interrupt, exitHandler(req, res, next))
               )
-            }
         )
+      )
     )
   }
 
@@ -167,21 +170,21 @@ export function LiveExpress(host: string, port: number): L.Layer<unknown, never,
 export function LiveExpress<R>(
   host: string,
   port: number,
-  exitHandler: (req: Request, res: Response, next: NextFunction) => (cause: Cause<never>) => T.URIO<R, void>
+  exitHandler: (req: Request, res: Response, next: NextFunction) => (cause: Cause<never>) => IO.URIO<R, void>
 ): L.Layer<R, never, ExpressEnv>
 export function LiveExpress<R>(
   host: string,
   port: number,
-  exitHandler?: (req: Request, res: Response, next: NextFunction) => (cause: Cause<never>) => T.URIO<R, void>
+  exitHandler?: (req: Request, res: Response, next: NextFunction) => (cause: Cause<never>) => IO.URIO<R, void>
 ): L.Layer<R, never, ExpressEnv> {
   return LiveExpressAppConfig(host, port, exitHandler || defaultExitHandler)['>+>'](LiveExpressApp)
 }
 
-export const expressApp = T.asksService(ExpressApp)((_) => _.app)
+export const expressApp = IO.asksService(ExpressApp)((_) => _.app)
 
-export const expressServer = T.asksService(ExpressApp)((_) => _.server)
+export const expressServer = IO.asksService(ExpressApp)((_) => _.server)
 
-export const { app: withExpressApp, server: withExpressServer } = T.deriveAsksIO(ExpressApp)(['app', 'server'])
+export const { app: withExpressApp, server: withExpressServer } = IO.deriveAsksIO(ExpressApp)(['app', 'server'])
 
 export const methods = [
   'all',
@@ -225,58 +228,62 @@ export interface ParsedQs {
   [key: string]: undefined | string | string[] | ParsedQs | ParsedQs[]
 }
 
-export interface EffectRequestHandler<
+export interface IORequestHandler<
   R,
-  P = ParamsDictionary,
+  Route extends string = any,
+  P = RouteParameters<Route>,
   ResBody = any,
   ReqBody = any,
   ReqQuery = ParsedQs,
   Locals extends Record<string, any> = Record<string, any>
 > {
-  (req: Request<P, ResBody, ReqBody, ReqQuery, Locals>, res: Response<ResBody, Locals>, next: NextFunction): T.URIO<
+  (req: Request<P, ResBody, ReqBody, ReqQuery, Locals>, res: Response<ResBody, Locals>, next: NextFunction): IO.URIO<
     R,
     void
   >
 }
 
-export function expressRuntime<Handlers extends NonEmptyArray<EffectRequestHandler<any, any, any, any, any, any>>>(
-  handlers: Handlers
-): T.IO<
+type AnyIORequestHandler<R> = IORequestHandler<R, any, any, any, any, any, any>
+
+export type IORequestHandlerArray<R = any, Route extends string = any> = Array<
+  IORequestHandler<R, Route, RouteParameters<Route>>
+>
+
+export function expressRuntime<Handlers extends IORequestHandlerArray>(
+  handlers: never extends Handlers ? IORequestHandlerArray : Handlers
+): IO.IO<
   _R<
     {
-      [k in keyof Handlers]: [Handlers[k]] extends [EffectRequestHandler<infer R, any, any, any, any, any>]
-        ? T.URIO<R, void>
-        : never
+      [k in keyof Handlers]: [Handlers[k]] extends [AnyIORequestHandler<infer R>] ? IO.URIO<R, void> : never
     }[number]
   > &
     Has<ExpressApp>,
   never,
   ReadonlyArray<RequestHandler>
 > {
-  return T.asksServiceIO(ExpressApp)((_) => _.runtime(handlers))
+  return IO.asksServiceIO(ExpressApp)((_) => _.runtime(handlers))
 }
 
 export function match(method: Methods): {
-  <Handlers extends NonEmptyArray<EffectRequestHandler<any, any, any, any, any, any>>>(
-    path: PathParams,
-    ...handlers: Handlers
-  ): T.URIO<
+  <Route extends string, Handlers extends IORequestHandlerArray<any, Route>>(
+    path: Route,
+    ...handlers: never extends Handlers ? Array<IORequestHandler<any, Route, RouteParameters<Route>>> : Handlers
+  ): IO.URIO<
     ExpressEnv &
       _R<
         {
-          [k in keyof Handlers]: [Handlers[k]] extends [EffectRequestHandler<infer R, any, any, any, any, any>]
-            ? T.URIO<R, void>
-            : never
+          [k in keyof Handlers]: [Handlers[k]] extends [AnyIORequestHandler<infer R>] ? IO.URIO<R, void> : never
         }[number]
       >,
     void
   >
 } {
   return function (path, ...handlers) {
-    return expressRuntime(handlers)['|>'](
-      T.chain((expressHandlers) =>
+    return pipe(
+      expressRuntime(handlers as Array<IORequestHandler<any>>),
+      IO.chain((expressHandlers) =>
         withExpressApp((app) =>
-          T.succeedLazy(() => {
+          IO.succeedLazy(() => {
             app[method](path, ...expressHandlers)
           })
         )
@@ -289,9 +296,9 @@ export function defaultExitHandler(
   _req: Request,
   _res: Response,
   _next: NextFunction
-): (cause: Cause<never>) => T.URIO<unknown, void> {
+): (cause: Cause<never>) => IO.URIO<unknown, void> {
   return (cause) =>
-    T.succeedLazy(() => {
+    IO.succeedLazy(() => {
       if (halted(cause)) {
         console.error(defaultPrettyPrint(cause))
       }
@@ -299,44 +306,42 @@ export function defaultExitHandler(
     })
 }
 
-export function use<Handlers extends NonEmptyArray<EffectRequestHandler<any, any, any, any, any, any>>>(
+export function use<Handlers extends IORequestHandlerArray>(
   ...handlers: Handlers
-): T.URIO<
+): IO.URIO<
   ExpressEnv &
     _R<
       {
-        [k in keyof Handlers]: [Handlers[k]] extends [EffectRequestHandler<infer R, any, any, any, any, any>]
-          ? T.URIO<R, void>
-          : never
+        [k in keyof Handlers]: [Handlers[k]] extends [AnyIORequestHandler<infer R>] ? IO.URIO<R, void> : never
       }[number]
     >,
   void
 >
-export function use<Handlers extends NonEmptyArray<EffectRequestHandler<any, any, any, any, any, any>>>(
-  path: PathParams,
-  ...handlers: Handlers
-): T.URIO<
+export function use<Route extends string, Handlers extends IORequestHandlerArray<any, Route>>(
+  path: Route,
+  ...handlers: never extends Handlers ? IORequestHandlerArray<any, Route> : Handlers
+): IO.URIO<
   ExpressEnv &
     _R<
       {
-        [k in keyof Handlers]: [Handlers[k]] extends [EffectRequestHandler<infer R, any, any, any, any, any>]
-          ? T.URIO<R, void>
-          : never
+        [k in keyof Handlers]: [Handlers[k]] extends [AnyIORequestHandler<infer R>] ? IO.URIO<R, void> : never
       }[number]
     >,
   void
 >
-export function use(...args: NonEmptyArray<any>): T.URIO<ExpressEnv, void> {
-  return withExpressApp((app) => {
-    if (typeof args[0] === 'function') {
-      return T.chain_(expressRuntime(args), (expressHandlers) => T.succeedLazy(() => app.use(...expressHandlers)))
-    } else {
-      return T.chain_(
-        expressRuntime(args.slice(1) as unknown as NonEmptyArray<EffectRequestHandler<any, any, any, any, any, any>>),
-        (expressHandlers) => T.succeedLazy(() => app.use(args[0], ...expressHandlers))
-      )
-    }
-  })['|>'](T.asUnit)
+export function use(...args: Array<any>): IO.URIO<ExpressEnv, void> {
+  return pipe(
+    withExpressApp((app) => {
+      if (typeof args[0] === 'function') {
+        return IO.chain_(expressRuntime(args), (expressHandlers) => IO.succeedLazy(() => app.use(...expressHandlers)))
+      } else {
+        return IO.chain_(expressRuntime(args.slice(1) ?? []), (expressHandlers) =>
+          IO.succeedLazy(() => app.use(args[0], ...expressHandlers))
+        )
+      }
+    }),
+    IO.asUnit
+  )
 }
 
 export const all     = match('all')
@@ -371,9 +376,8 @@ export const unsubscribe = match('unsubscribe')
 /**
  * Lift an express requestHandler into an effectified variant
  */
-export function classic(_: NextHandleFunction, __trace?: string): EffectRequestHandler<unknown>
-export function classic(_: RequestHandler, __trace?: string): EffectRequestHandler<unknown>
-export function classic(_: RequestHandler | NextHandleFunction, __trace?: string): EffectRequestHandler<unknown> {
-  // @ts-expect-error
-  return (req, res, next) => T.succeedWith(() => _(req, res, next), __trace)
+export function classic(_: NextHandleFunction): IORequestHandler<unknown, any>
+export function classic(_: RequestHandler): IORequestHandler<unknown, any>
+export function classic(_: RequestHandler | NextHandleFunction): IORequestHandler<unknown, any> {
+  return (req, res, next) => IO.succeedLazy(() => _(req, res, next))
 }
