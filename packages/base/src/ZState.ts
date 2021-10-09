ce6d6d5262310e4f976668de13869a0c9b7f78b5
0x706b <peter@peterkrol.com>
2021-10-09 18:27:55 -0400

    feat(base/zstate): add `dataFirst` annotations

--- packages/base/src/ZState.ts
+++ packages/base/src/ZState.ts
@@ -22,18 +22,27 @@ export const modify: <S, A>(f: (s: S) => readonly [A, S]) => ZState<S, A> = Z.mo
 
 export const gets: <S, A>(f: (s: S) => A) => ZState<S, A> = Z.gets
 
-export const getsM: <S, A>(f: (s: S) => ZState<S, A>) => ZState<S, A> = Z.getsZ
+export const getsZ: <S, A>(f: (s: S) => ZState<S, A>) => ZState<S, A> = Z.getsZ
 
 export const execute_: <S, A>(ma: ZState<S, A>, s: S) => S = Z.runState_
 
+/**
+ * @dataFirst execute
+ */
 export const execute: <S>(s: S) => <A>(ma: ZState<S, A>) => S = Z.runState
 
 export const evaluate_: <S, A>(ma: ZState<S, A>, s: S) => A = Z.runStateResult_
 
+/**
+ * @dataFirst evaluate_
+ */
 export const evaluate: <S>(s: S) => <A>(ma: ZState<S, A>) => A = Z.runStateResult
 
 export const run_: <S, A>(ma: ZState<S, A>, s: S) => readonly [S, A] = Z.run_
 
+/**
+ * @dataFirst run_
+ */
 export const run: <S>(s: S) => <A>(ma: ZState<S, A>) => readonly [S, A] = Z.run
 
 /*
@@ -52,20 +61,32 @@ export const pure: <S = never, A = never>(a: A) => ZState<S, A> = Z.pure
 
 export const cross_: <S, A, B>(fa: ZState<S, A>, fb: ZState<S, B>) => ZState<S, readonly [A, B]> = Z.cross_
 
+/**
+ * @dataFirst cross_
+ */
 export const cross: <S, B>(fb: ZState<S, B>) => <A>(fa: ZState<S, A>) => ZState<S, readonly [A, B]> = Z.cross
 
 export const crossWith_: <S, A, B, C>(fa: ZState<S, A>, fb: ZState<S, B>, f: (a: A, b: B) => C) => ZState<S, C> =
   Z.crossWith_
 
+/**
+ * @dataFirst crossWith_
+ */
 export const crossWith: <S, A, B, C>(fb: ZState<S, B>, f: (a: A, b: B) => C) => (fa: ZState<S, A>) => ZState<S, C> =
   Z.crossWith
 
 export const crossSecond_: <S, A, B>(fa: ZState<S, A>, fb: ZState<S, B>) => ZState<S, B> = Z.crossSecond_
 
+/**
+ * @dataFirst crossSecond_
+ */
 export const crossSecond: <S, B>(fb: ZState<S, B>) => <A>(fa: ZState<S, A>) => ZState<S, B> = Z.crossSecond
 
 export const crossFirst_: <S, A, B>(fa: ZState<S, A>, fb: ZState<S, B>) => ZState<S, A> = Z.crossFirst_
 
+/**
+ * @dataFirst crossFirst_
+ */
 export const crossFirst: <S, B>(fb: ZState<S, B>) => <A>(fa: ZState<S, A>) => ZState<S, A> = Z.crossFirst
 
 /*
@@ -84,6 +105,9 @@ export const unit: <S>() => ZState<S, void> = Z.unit
 
 export const ap_: <S, A, B>(fab: ZState<S, (a: A) => B>, fa: ZState<S, A>) => ZState<S, B> = Z.ap_
 
+/**
+ * @dataFirst ap_
+ */
 export const ap: <S, A>(fa: ZState<S, A>) => <B>(fab: ZState<S, (a: A) => B>) => ZState<S, B> = Z.ap
 
 /*
@@ -94,6 +118,9 @@ export const ap: <S, A>(fa: ZState<S, A>) => <B>(fab: ZState<S, (a: A) => B>) =>
 
 export const map_: <S, A, B>(fa: ZState<S, A>, f: (a: A) => B) => ZState<S, B> = Z.map_
 
+/**
+ * @dataFirst map_
+ */
 export const map: <A, B>(f: (a: A) => B) => <S>(fa: ZState<S, A>) => ZState<S, B> = Z.map
 
 /*
@@ -104,12 +131,18 @@ export const map: <A, B>(f: (a: A) => B) => <S>(fa: ZState<S, A>) => ZState<S, B
 
 export const chain_: <S, A, B>(ma: ZState<S, A>, f: (a: A) => ZState<S, B>) => ZState<S, B> = Z.chain_
 
+/**
+ * @dataFirst chain_
+ */
 export const chain: <S, A, B>(f: (a: A) => ZState<S, B>) => (ma: ZState<S, A>) => ZState<S, B> = Z.chain
 
 export const flatten: <S, A>(mma: ZState<S, ZState<S, A>>) => ZState<S, A> = Z.flatten
 
 export const tap_: <S, A, B>(ma: ZState<S, A>, f: (a: A) => ZState<S, B>) => ZState<S, A> = Z.tap_
 
+/**
+ * @dataFirst tap_
+ */
 export const tap: <S, A, B>(f: (a: A) => ZState<S, B>) => (ma: ZState<S, A>) => ZState<S, A> = Z.tap
 
 /*
