abaf38c141950380e1cff1edbf49876da9f7425f
0x706b <peter@peterkrol.com>
2021-10-09 13:38:54 -0400

    feat(base/eq): add `dataFirst` annotations

--- packages/base/src/Eq.ts
+++ packages/base/src/Eq.ts
@@ -65,6 +65,9 @@ export function contramap_<A, B>(fa: Eq<A>, f: (b: B) => A): Eq<B> {
   return Eq((x, y) => fa.equals_(f(x), f(y)))
 }
 
+/**
+ * @dataFirst contramap_
+ */
 export function contramap<A, B>(f: (b: B) => A): (fa: Eq<A>) => Eq<B> {
   return (fa) => contramap_(fa, f)
 }
