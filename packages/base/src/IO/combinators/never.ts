15c9f7b5b71780aac109991475654b441ed96575
0x706b <peter@peterkrol.com>
2021-12-02 21:45:14 -0500

    fix(io): revert `never` change

--- packages/base/src/IO/combinators/never.ts
+++ packages/base/src/IO/combinators/never.ts
@@ -2,12 +2,20 @@
 
 import type { UIO } from '../core'
 
-import { async } from '../core'
+import { defer, succeedLazy } from '../core'
+import { asyncInterrupt } from './interrupt'
 
 /**
  * Returns a `IO` that will never produce anything. The moral equivalent of
  * `while(true) {}`, only without the wasted CPU cycles.
  */
-export const never: UIO<never> = async(() => {
-  //
-})
+export const never: UIO<never> = defer(() =>
+  asyncInterrupt<unknown, never, never>(() => {
+    const interval = setInterval(() => {
+      //
+    }, 60000)
+    return succeedLazy(() => {
+      clearInterval(interval)
+    })
+  })
+)
